6336ce065cb0bb482bb345f3777dafb7a9215814
AMR856 <amer.live477@gmail.com>
2023-08-18 19:48:22 +0300

    Added first main.h

---
+++ main.h
@@ -0,0 +1,20 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * struct MyStruct - A struct to manipulate opeartions and functions
+ * @op: The specifier of the printf function
+ * @myFunc: Pointer to the function that will be called
+ * after checking the op
+*/
+
+typedef struct MyStruct
+{
+	char *op;
+	int (*myFunc);
+} op_t;
+
+#endif
